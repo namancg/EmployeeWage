a1976c5dc692646e5858f6362ba361a53705ae6e
namancg <naman.c@perfios.com>
2021-08-21 00:47:38 +0530

    ADDED: uc4-switch

--- src/com/bridgelabz/employeewagecalculator/employeewagecalculator.java
+++ src/com/bridgelabz/employeewagecalculator/employeewagecalculator.java
@@ -3,22 +3,23 @@ package com.bridgelabz.employeewagecalculator;
 public class employeewagecalculator {
 
 	public static void main(String[] args) {
-		int PART = 1;
-        int FULL = 2;
+		final int PART = 1;
+        final int FULL = 2;
         int WagePH = 20;
         int empType = (int) (Math.random() * 100) % 3;
-        int workingHours = 0;
-        if (empType == FULL)
+        int workingHours =0;
+        switch (empType)
         {
-            System.out.println("Employee is Present Full time");
-            workingHours = 8;
-        } else if (empType == PART)
-        {
-            System.out.println("Employee is Present Part time");
-            workingHours = 4;
-        } else
-        {
-            System.out.println("Employee is Absent");
+            case FULL:
+                System.out.println("Employee is Present Full time");
+                workingHours = 8;
+                break;
+            case PART:
+                System.out.println("Employee is Present Part time");
+                workingHours = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent");    
         }
         int wage = workingHours * WagePH;
         System.out.println("Employee Daily Wage is " + wage);
