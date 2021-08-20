8bb5fdc55fe1ec1052156639afcb3f0070ccbbd9
namancg <naman.c@perfios.com>
2021-08-21 00:24:46 +0530

    ADDED: uc2 - daily wages

--- src/com/bridgelabz/employeewagecalculator/employeewagecalculator.java
+++ src/com/bridgelabz/employeewagecalculator/employeewagecalculator.java
@@ -3,17 +3,20 @@ package com.bridgelabz.employeewagecalculator;
 public class employeewagecalculator {
 
 	public static void main(String[] args) {
-		int FULL = 1;
+		final int FULL = 1;
+        final int WagePH = 30;
         int emp = (int) (Math.random() * 100) % 2;
+        int workingHours = 0;
         if (emp == FULL)
         {
             System.out.println("Employee is Present");
-
+            workingHours = 8;
         } else
         {
             System.out.println("Employee is Absent");
-
         }
+        int wage = workingHours * WagePH;
+        System.out.println("Employee Daily Wage is " + wage);
 
 	}
 
