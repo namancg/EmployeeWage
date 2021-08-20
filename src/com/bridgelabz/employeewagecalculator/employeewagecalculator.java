e47cfa46b8b55e44f370f132a211bd9eb4330b41
namancg <naman.c@perfios.com>
2021-08-21 00:51:44 +0530

    ADDED: uc5-MonthlyWages

--- src/com/bridgelabz/employeewagecalculator/employeewagecalculator.java
+++ src/com/bridgelabz/employeewagecalculator/employeewagecalculator.java
@@ -5,24 +5,29 @@ public class employeewagecalculator {
 	public static void main(String[] args) {
 		final int PART = 1;
         final int FULL = 2;
-        int WagePH = 20;
-        int empType = (int) (Math.random() * 100) % 3;
-        int workingHours =0;
-        switch (empType)
-        {
-            case FULL:
-                System.out.println("Employee is Present Full time");
-                workingHours = 8;
-                break;
-            case PART:
-                System.out.println("Employee is Present Part time");
-                workingHours = 4;
-                break;
-            default:
-                System.out.println("Employee is Absent");    
-        }
-        int wage = workingHours * WagePH;
-        System.out.println("Employee Daily Wage is " + wage);
+        int WagePH = 30;
+        int WORKING_DAYS = 20;
+   
+       int totalWage = 0;
+       for (int day = 1; day<= WORKING_DAYS; day++)
+       {
+           int emp = (int) (Math.random() * 100) % 3;
+           int workingHours =0;
+           switch (emp)
+           {
+               case FULL:
+                   workingHours = 8;
+                   break;
+               case PART:
+                   workingHours = 4;
+                   break;
+               default:
+           }
+           int wage = workingHours * WagePH;
+           System.out.println("Day " + day + " wage is=" + wage);
+           totalWage += wage;
+       }
+       System.out.println("Total wage for a month is =" + totalWage);  
 
 	}
 
